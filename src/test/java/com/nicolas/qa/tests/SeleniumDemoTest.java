e385bcbb5949e55bb65e49ea6ea9d148cd59e784
Nicolas <ntrujilloc@ucentral.edu.co>
2025-07-24 17:09:14 -0500

    Optimizacion de codigo general

--- src/test/java/com/nicolas/qa/tests/SeleniumDemoTest.java
+++ src/test/java/com/nicolas/qa/tests/SeleniumDemoTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.security.Key;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -22,55 +24,74 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SeleniumDemoTest {
 
-    @Test
-    public void abrirPagina(){
+    public WebDriver setupDriver(){
         System.setProperty("webdriver.edge.driver","C:\\WebDriver\\msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        return driver;
+    }
 
+    public void loginTest(WebDriver driver, String username, String password) {
         driver.get("https://www.saucedemo.com");
 
-        WebElement username = driver.findElement(By.id("user-name"));
-        WebElement password = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.id("login-button"));
-
-        username.sendKeys("standard_user");
-        password.sendKeys("secret_sauce");
-        loginButton.click();
-
-        new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.urlContains("inventory"));
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login-button")).click();
+ 
+    }
         
+    @Test
+    public void inventoryPageTest(){
+
+        loginTest(driver, "standard_user","secret_sauce");
+
+        new WebDriverWait(driver, Duration.ofSeconds(1)).until(ExpectedConditions.urlContains("inventory"));   
+    
         WebElement productList = driver.findElement(By.id("inventory_container"));
+        WebElement name = driver.findElement(By.className("inventory_item_name"));
+        WebElement price = driver.findElement(By.className("inventory_item_price"));
         List<WebElement> products = driver.findElements(By.className("inventory_item"));
 
         assertTrue(driver.getCurrentUrl().toLowerCase().contains("inventory"));
         assertTrue(productList.isDisplayed());
         assertEquals(6, products.size());
+        assertEquals("Sauce Labs Backpack",name.getText());
+        assertEquals("$29.99",price.getText());
 
-        driver.quit();
+
     }
 
-    
     @ParameterizedTest
     @CsvSource({
         "usuario_invalido1, clave_invalida1",
         "usuario_invalido2, clave_invalida2",
-        "locked_out_user, secret_sauce"
+        "usuario_invalido3, secret_sauce"
     })
     public void wrongLogin(String username, String password){
-        System.setProperty("webdriver.edge.driver", "C:\\WebDriver\\msedgedriver.exe");
-        WebDriver driver = new EdgeDriver();
 
-        driver.get("https://www.saucedemo.com");
+        loginTest(driver, username, password);
+
+        WebElement errorMessage = new WebDriverWait(driver, Duration.ofSeconds(1)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3[data-test='error']")));
         
-        driver.findElement(By.id("user-name")).sendKeys(username);
-        driver.findElement(By.id("password")).sendKeys(password);
-        driver.findElement(By.id("login-button")).click();
+        //assertTrue(errorMessage.getText().toLowerCase().contains("username and password"));
+        String errorText = errorMessage.getText().toLowerCase();
 
-        WebElement errorMessage = new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3[data-test='error']")));
-        
-        assertTrue(errorMessage.getText().toLowerCase().contains("username and password"));
+        assertTrue(errorText.contains("password")
+                || errorText.contains("username"));
 
-        driver.quit();
+    }
+
+    WebDriver driver;
+    @BeforeEach
+    void setup() {
+        driver = setupDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
 
